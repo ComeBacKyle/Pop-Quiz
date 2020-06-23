61dd25d2a6579d081c3ea33c50cb5c70dde912dc
Aaron Jamal <aaron.jamal09@gmail.com>
2020-06-22 21:45:59 -0400

    Try again again

---
+++ script.js
@@ -0,0 +1,16 @@
+var start = document.addEventListener("click", startButton)
+
+
+function doneButton() {
+
+}
+
+function answerButton() {
+
+}
+
+function startButton() {
+    $("button.start").html("html1")
+
+
+}
